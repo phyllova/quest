ecdeba5938753ba7e88f6edae26143b99304cba9
Phy Llova <phyllova@acelinkcourier.com>
2023-11-26 14:50:49 -0800

    added files

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDhn80byD69jCo4oexdKdkCHUgGw9nO6CM",
-  authDomain: "dubai4db.firebaseapp.com",
-  databaseURL: "https://dubai4db-default-rtdb.firebaseio.com",
-  projectId: "dubai4db",
-  storageBucket: "dubai4db.appspot.com",
-  messagingSenderId: "776520121975",
-  appId: "1:776520121975:web:248815949672b2c4371072"
+  apiKey: "AIzaSyCBXpUYM4uAYl1lKl2ItpB1oDsc4c1n67s",
+  authDomain: "ravedasians.firebaseapp.com",
+  databaseURL: "https://ravedasians-default-rtdb.firebaseio.com",
+  projectId: "ravedasians",
+  storageBucket: "ravedasians.appspot.com",
+  messagingSenderId: "213183266988",
+  appId: "1:213183266988:web:769425f520bdb201231f09"
 };
 firebase.initializeApp(firebaseConfig);
 const appCheck = firebase.appCheck();
